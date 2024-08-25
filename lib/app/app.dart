20fa55ac5c2dca67377d6a0b4a7a09af6fdb2f9f
5122022YV <ydorozhko@yandex.ru>
2024-08-25 11:06:46 +0300

    after_created_avto_login

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,20 +1,55 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:zc_dodiddone/pages/main_page.dart';
+import 'package:zc_dodiddone/services/firebase_auth.dart';
 import '../pages/login_page.dart';
 import 'package:zc_dodiddone/theme/theme.dart';
 
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FirebaseAuthService _authService = FirebaseAuthService();
+  late User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    user = _authService.currentUser; // Initialize user here
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'zc_dodiddone',
+      title: 'Flutter Demo',
       theme: DoDidDoneTheme.lightTheme,
-      home: const LoginPage(),
-    );
+      home: user == null ? const LoginPage() : const MainPage(),
+    ); 
   }
 }
 
+// Используемый код до изменений для автовхода 
+// import 'package:flutter/material.dart';
+// import '../pages/login_page.dart';
+// import 'package:zc_dodiddone/theme/theme.dart';
 
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'zc_dodiddone',
+//       theme: DoDidDoneTheme.lightTheme,
+//       home: const LoginPage(),
+//     );
+//   }
+// }
